c6a55ddd53250e525d9a9ef9256cd5797e23d3c0
Rohit Kumar <126140517+VnsRohit@users.noreply.github.com>
2023-10-23 22:11:19 +0530

    Update main.py

--- Iss_Api/main.py
+++ Iss_Api/main.py
@@ -53,4 +53,4 @@ while True:
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+# run the code every 60 seconds.
